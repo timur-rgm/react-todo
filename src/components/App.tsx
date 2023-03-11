8676c82bb35d2fb2ac2a8760af1457e257b842b7
Timur <tiabumuaz@gmail.com>
2023-03-11 20:02:36 +0500

    Сверстан список задач

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,7 @@
 import styles from "./App.module.scss";
+import checkmarkIcon from "../assets/images/checkmark-icon.svg";
+import editIcon from "../assets/images/edit-icon.svg";
+import closeIcon from "../assets/images/close-icon.svg";
 
 function App() {
   return (
@@ -14,6 +17,104 @@ function App() {
             Добавить
           </button>
         </form>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            <img
+              className={styles.completeIcon}
+              src={checkmarkIcon}
+              alt="Иконка выполнено"
+              width={22}
+              height={22}
+            />
+            Разработка архитектуры проекта
+            <img
+              className={styles.editIcon}
+              src={editIcon}
+              alt="Иконка редактировать"
+              width={15}
+              height={15}
+            />
+            <img
+              className={styles.closeIcon}
+              src={closeIcon}
+              alt="Иконка удалить"
+              width={22}
+              height={22}
+            />
+          </li>
+          <li className={styles.listItem}>
+            <img
+              className={styles.completeIcon}
+              src={checkmarkIcon}
+              alt="Иконка выполнено"
+              width={22}
+              height={22}
+            />
+            Разворачивание и старт проекта
+            <img
+              className={styles.editIcon}
+              src={editIcon}
+              alt="Иконка редактировать"
+              width={15}
+              height={15}
+            />
+            <img
+              className={styles.closeIcon}
+              src={closeIcon}
+              alt="Иконка удалить"
+              width={22}
+              height={22}
+            />
+          </li>
+          <li className={styles.listItem}>
+            <img
+              className={styles.completeIcon}
+              src={checkmarkIcon}
+              alt="Иконка выполнено"
+              width={22}
+              height={22}
+            />
+            Пробелы в теории
+            <img
+              className={styles.editIcon}
+              src={editIcon}
+              alt="Иконка редактировать"
+              width={15}
+              height={15}
+            />
+            <img
+              className={styles.closeIcon}
+              src={closeIcon}
+              alt="Иконка удалить"
+              width={22}
+              height={22}
+            />
+          </li>
+          <li className={styles.listItem}>
+            <img
+              className={styles.completeIcon}
+              src={checkmarkIcon}
+              alt="Иконка выполнено"
+              width={22}
+              height={22}
+            />
+            Заплатить за свет
+            <img
+              className={styles.editIcon}
+              src={editIcon}
+              alt="Иконка редактировать"
+              width={15}
+              height={15}
+            />
+            <img
+              className={styles.closeIcon}
+              src={closeIcon}
+              alt="Иконка удалить"
+              width={22}
+              height={22}
+            />
+          </li>
+        </ul>
       </div>
     </div>
   );
