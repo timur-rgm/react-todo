a9dbf0ca60f5db405236b1ea98fb2c18443e987d
Timur <tiabumuaz@gmail.com>
2023-03-24 13:29:27 +0500

    Добавлена валидация input

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,9 +3,11 @@ import { useDispatch } from "react-redux";
 import { addTodo } from "../../store/todo/todoSlice";
 import { createTodo } from "../../helpers/createTodo";
 import styles from "./Form.module.scss";
+import cn from "classnames";
 
 function Form() {
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const dispatch = useDispatch();
 
@@ -15,15 +17,26 @@ function Form() {
 
   const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
+
+    if (!inputValue.trim()) {
+      setIsValid(false);
+      setInputValue("");
+      return;
+    }
+
     const newTodo = createTodo(inputValue);
     dispatch(addTodo(newTodo));
     setInputValue("");
+    setIsValid(true);
   };
 
   return (
     <form className={styles.form} onSubmit={handleFormSubmit}>
+      {!isValid && (
+        <p className={styles.errorMessage}>Поле не может быть пустым</p>
+      )}
       <input
-        className={styles.input}
+        className={cn(styles.input, { [styles.invalid]: !isValid })}
         type="text"
         placeholder="Придумайте задачу"
         value={inputValue}
